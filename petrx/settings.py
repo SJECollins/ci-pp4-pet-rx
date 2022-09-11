0111d7033e37b646dd9d86cc10a8256f2a4e4fae
SCollins <sje.collins@gmail.com>
2022-09-11 12:48:05 +0000

    Fix link to static in settings.py

--- petrx/settings.py
+++ petrx/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = {os.path.join(BASE_DIR, 'static')}
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIAL_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
