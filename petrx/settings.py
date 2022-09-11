fef585ccdea89e8bceb54b6eb67250bcc1aed92f
SCollins <sje.collins@gmail.com>
2022-09-11 15:42:38 +0000

    Add If/else to nav for authentication. Style nav, links, buttons

--- petrx/settings.py
+++ petrx/settings.py
@@ -134,9 +134,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIAL_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
