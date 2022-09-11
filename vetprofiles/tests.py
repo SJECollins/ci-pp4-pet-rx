e0d7acf61a8d439654399561d10eea7bcc829067
SCollins <sje.collins@gmail.com>
2022-09-11 20:19:26 +0000

    Initial testing - error in create_superuser

--- vetprofiles/tests.py
+++ vetprofiles/tests.py
@@ -1,3 +1,46 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(email='normal@user.com', first_name='bob', last_name='bobberson', password='foo')
+        self.assertEqual(user.email, 'normal@user.com')
+        self.assertEqual(user.first_name, 'bob')
+        self.assertEqual(user.last_name, 'bobberson')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        try:
+            # username is None for the AbstractUser option
+            # username does not exist for the AbstractBaseUser option
+            self.assertIsNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', first_name='bob', last_name='bobberson', password="foo")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email='super@user.com', first_name='bob', last_name='bobberson', password='foo')
+        self.assertEqual(admin_user.email, 'super@user.com')
+        self.assertEqual(admin_user.first_name, 'bob')
+        self.assertEqual(admin_user.last_name, 'bobberson')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            # username is None for the AbstractUser option
+            # username does not exist for the AbstractBaseUser option
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='super@user.com', first_name='bob', last_name='bobberson', password='foo', is_superuser=False)
