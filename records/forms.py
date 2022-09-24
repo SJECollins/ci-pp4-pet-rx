256f5adacb06bb3abd05d03eb95482bcec124d1c
SCollins <sje.collins@gmail.com>
2022-09-24 19:11:40 +0000

    Add datepicker to add_animal form

--- records/forms.py
+++ records/forms.py
@@ -3,10 +3,17 @@ from .models import Record
 from django.core.exceptions import NON_FIELD_ERRORS
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class RecordForm(forms.ModelForm):
     class Meta:
         model = Record
         fields = ['name', 'surname', 'date_of_birth', 'species', 'breed', 'sex', 'weight']
+        widgets = {
+            'date_of_birth': DateInput(),
+        }
         error_messages = {
             NON_FIELD_ERRORS: {
                 'unique_together': "A record already exists with the same %(field_labels)s.",
