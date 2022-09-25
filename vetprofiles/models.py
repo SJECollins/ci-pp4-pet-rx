68dd007efdb7e5fe2ef680826ab88c80e7219f97
SCollins <sje.collins@gmail.com>
2022-09-25 15:12:16 +0000

    Add brief comments to HTML

--- vetprofiles/models.py
+++ vetprofiles/models.py
@@ -42,7 +42,6 @@ class Vet(AbstractBaseUser):
     last_login = models.DateTimeField(verbose_name='last login', auto_now=True)
     # Have to be added for custom user models
     is_admin = models.BooleanField(default=False)
-    # Setting to false, admin have to approve registration!
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
