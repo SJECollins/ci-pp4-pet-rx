9cbe9a2273858fb3e0e536232e012b106fca5f60
SCollins <sje.collins@gmail.com>
2022-09-30 16:50:44 +0000

    Edit sendmail.js and collectstatic

--- static/js/sendmail.js
+++ static/js/sendmail.js
@@ -1,11 +1,12 @@
-let sender = document.getElementById('contact_name').value
-let email = document.getElementById('contact_email').value
-let message = document.getElementById('contact_message').value
 let sendMail = document.getElementById('send-mail') 
 
 sendMail.addEventListener('click', function(event){
-    event.preventDefault()
-    emailjs.send("contact_service", "contact_form", {
+
+    let sender = document.getElementById('contact_name').value
+    let email = document.getElementById('contact_email').value
+    let message = document.getElementById('contact_message').value
+    
+    emailjs.sendForm("contact_service", "contact_form", {
         "contact_name": sender,
         "contact_email": email,
         "contact_message": message,
