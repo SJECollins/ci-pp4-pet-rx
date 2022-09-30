9dc3f4de8e7293a8db0b242eb592432b493a8650
SCollins <sje.collins@gmail.com>
2022-09-30 17:43:39 +0000

    Edit contact to include onsubmit, from CI bootstrap module

--- static/js/sendmail.js
+++ static/js/sendmail.js
@@ -1,22 +1,21 @@
-let sendMail = document.getElementById('send-mail') 
+function sendMail() {
 
-sendMail.addEventListener('click', function(event){
+    let sender = document.getElementById("contact_name").value
+    let email = document.getElementById("contact_email").value
+    let message = document.getElementById("contact_message").value
 
-    let sender = document.getElementById('contact_name').value
-    let email = document.getElementById('contact_email').value
-    let message = document.getElementById('contact_message').value
-    
     emailjs.sendForm("contact_service", "contact_form", {
         "contact_name": sender,
         "contact_email": email,
-        "contact_message": message,
+        "contact_message": message
     })
-        .then(
-            function (response) {
-                console.log("SUCCESS", response.status);
-            },
-            function (error) {
-                console.log("FAILED", error);  
-            }
-        );
-})
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
