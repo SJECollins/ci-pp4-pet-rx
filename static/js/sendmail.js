7f4ab8b13ba917de1866dc2a728f2261255a3596
SCollins <sje.collins@gmail.com>
2022-09-30 16:36:04 +0000

    Add response param

---
+++ static/js/sendmail.js
@@ -0,0 +1,21 @@
+let sender = document.getElementById('contact_name').value
+let email = document.getElementById('contact_email').value
+let message = document.getElementById('contact_message').value
+let sendMail = document.getElementById('send-mail') 
+
+sendMail.addEventListener('click', function(event){
+    event.preventDefault()
+    emailjs.send("contact_service", "contact_form", {
+        "contact_name": sender,
+        "contact_email": email,
+        "contact_message": message,
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);  
+            }
+        );
+})
