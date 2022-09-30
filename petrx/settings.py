504b789bf26515ceb026d4a58f1ec81b8e934b38
SCollins <sje.collins@gmail.com>
2022-09-30 14:56:57 +0000

    Fix typo in settings.py for EMAIL_BACKEND

--- petrx/settings.py
+++ petrx/settings.py
@@ -172,7 +172,7 @@ if development:
     EMAIL_PORT = '2525'
 else:
     # Gmail
-    EMAIL_BACKEND = 'django.core.mail.backends.core.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 597
     EMAIL_HOST = 'smtp.gmail.com'
