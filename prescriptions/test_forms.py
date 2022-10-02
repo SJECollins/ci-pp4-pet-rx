bd4f1aed31d2453574456b51c45273ac70c1fdd5
SCollins <sje.collins@gmail.com>
2022-10-02 14:19:27 +0000

    Start prescriptions.forms tests

--- prescriptions/test_forms.py
+++ prescriptions/test_forms.py
@@ -1,4 +1,44 @@
 from django.test import TestCase
-from .models import Prescription
+from .models import Drug
 from .forms import PrescrForm
 
+
+class TestPrescrForm(TestCase):
+    """
+    Testing prescription form for required fields.
+    """
+    def setUp(self):
+        self.drug = Drug.objects.create(
+            name='Amoxiclav',
+            dose=12.5,
+            measure='mg',
+            category='Antibiotic',
+            route='PO',
+            warnings='None'
+        )
+        self.drug.save()
+
+    def test_empty_fields(self):
+        """
+        Empty drug field should return error.
+        Frequency and length can be empty.
+        """
+        form = PrescrForm(data={
+            'drug': '',
+            'frequency': '',
+            'length': '',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['drug'][0], 'This field is required.')
+
+    def test_valid_form(self):
+        """
+        Form is valid if drug field filled with drug entry.
+        Form can be valid if frequency and length not selected.
+        """
+        form = PrescrForm(data={
+            'drug': self.drug,
+            'frequency': '',
+            'length': '',
+        })
+        self.assertTrue(form.is_valid())
