4dd47b91440eae741125d608025b2f7a1f3dd298
SCollins <sje.collins@gmail.com>
2022-10-02 21:08:05 +0000

    Remove unused ValidationError

--- vetprofiles/test_forms.py
+++ vetprofiles/test_forms.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.core.exceptions import ValidationError
 from .models import Vet
 from .forms import ContactForm, RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
 
