bac10c2a259e77b7db302a5eeda262c866170b44
SCollins <sje.collins@gmail.com>
2022-10-06 13:46:20 +0000

    Edit Drug and Prescription models to calculate tablets for prescription

--- prescriptions/models.py
+++ prescriptions/models.py
@@ -10,6 +10,7 @@ ROUTES = (('PO', 'PO'), ('IV', 'IV'), ('IM', 'IM'),
 FREQUENCY = (('No Repeat', 'No Repeat'), ('SID', 'SID'),
              ('BID', 'BID'), ('TID', 'TID'))
 MEASURE = (('ml', 'ml'), ('mg', 'mg'))
+TYPES = (('Injectable', 'Injectable'), ('Liquid', 'Liquid'), ('Tablet', 'Tablet'))
 
 
 class Category(models.Model):
@@ -37,7 +38,10 @@ class Drug(models.Model):
     Ordered by name by default.
     """
     name = models.CharField(max_length=50)
+    type = models.CharField(max_length=10, choices=TYPES, default='Injectable')
     dose = models.DecimalField(max_digits=4, decimal_places=2)
+    high_dose = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    tablet_strength = models.CharField(max_length=100, blank=True)
     measure = models.CharField(max_length=2, choices=MEASURE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     route = models.CharField(max_length=8, choices=ROUTES)
@@ -68,10 +72,11 @@ class Prescription(models.Model):
         max_digits=5, decimal_places=2, blank=True, null=True)
     vet = models.ForeignKey(Vet, on_delete=models.CASCADE, default=1)
     drug = models.ForeignKey(Drug, on_delete=models.CASCADE, default=None)
+    type = models.CharField(max_length=10, blank=True)
     drug_dose = models.DecimalField(
         max_digits=4, decimal_places=2, blank=True, null=True)
-    dose = models.DecimalField(
-        max_digits=5, decimal_places=2, blank=True, null=True)
+    drug_dose_high = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    dose = models.CharField(max_length=100, blank=True, null=True)
     measure = models.CharField(max_length=2, blank=True)
     frequency = models.CharField(max_length=10, choices=FREQUENCY, blank=True)
     length = models.PositiveIntegerField(default=0, blank=True)
@@ -94,11 +99,31 @@ class Prescription(models.Model):
         based on animal and drug.
         Calculates dose by animal weight and drug dose, saves.
         """
-        self.animal_weight = self.animal.weight
+        self.type = self.drug.type
         self.drug_dose = self.drug.dose
-        self.dose = self.animal_weight * self.drug_dose
+        self.drug_dose_high = self.drug.high_dose
+        self.animal_weight = self.animal.weight
         self.route = self.drug.route
         self.measure = self.drug.measure
+
+        if self.type == 'Injectable' or self.type == 'Liquid':
+            self.dose = f"{self.animal_weight * self.drug_dose}"
+        elif self.type == 'Tablet':
+            strengths = [int(num) for num in self.drug.tablet_strength.split(',') if num.strip().isdigit()]
+            quartstrengths = [strength / 4 for strength in strengths]
+            low = int(self.drug_dose * self.animal_weight)
+            high = int(self.drug_dose_high * self.animal_weight)
+            results = {'numtabs': 0, 'strength': 0}
+            for num in range(low, high):
+                size_list = []
+                for quart in quartstrengths:
+                    if num % quart == 0:
+                        size_list.append(num)
+                        smallest = min(size_list) / quart
+                        if results['numtabs'] == 0 or smallest < results['numtabs']:
+                            results['numtabs'] = smallest
+                            results['strength'] = quart
+            self.dose = f"{results['numtabs'] / 4}tabs x {int(results['strength'] * 4)}"
         super().save(*args, **kwargs)
 
     @property
