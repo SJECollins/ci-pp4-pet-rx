9b111b7a43b509738a221b1ba496f12da0ee7dc2
SCollins <sje.collins@gmail.com>
2022-10-19 12:43:02 +0000

    Add custom scrollbar for prescription lists

--- petrx/settings.py
+++ petrx/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 
 if os.path.isfile('env.py'):
-    import env  # noqa
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
